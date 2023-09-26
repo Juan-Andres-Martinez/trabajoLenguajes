c1c1b18347a4d84fa6a998c1d42d099e3c9aff76
Juan-Andres-Martinez <109612102+Juan-Andres-Martinez@users.noreply.github.com>
2023-09-25 23:53:37 -0500

    Add files via upload

---
+++ inicioLenguajes/inicioLenguajes/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace inicioLenguajes
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conexion = new MySqlConnection("Server=localhost;Database=lenguajes;user=juan;password=123456;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = textBoxUsuario.Text;
+            string contraseña = textBox2.Text;
+            try
+            {
+                conexion.Open();
+                string consulta = "SELECT COUNT(*) FROM usuarios WHERE usuario = @usuario AND contraseña = @contraseña";
+                MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+                int resultado = Convert.ToInt32(comando.ExecuteScalar());
+                if (resultado > 0)  
+                {
+                    MessageBox.Show("Inicio de sesión exitoso.");
+
+                    Form2 nuevoFormulario = new Form2();
+                    nuevoFormulario.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos.");
+                    textBoxUsuario.Clear();
+                    textBox2.Clear();
+
+                }
+
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBoxUsuario.Clear();
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ inicioLenguajes/inicioLenguajes/Form3.cs
@@ -0,0 +1,83 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inicioLenguajes
+{
+    public partial class Form3 : Form
+    {
+        private MySqlConnection conexion = new MySqlConnection("Server=localhost;Database=lenguajes;user=juan;password=123456;");
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = textBox1.Text;
+            string usuario = textBox2.Text;
+            string correo = textBox3.Text;
+            string contraseña = textBox4.Text;
+            try 
+            { 
+                conexion.Open();
+                string consulta = "INSERT INTO usuarios (nombre, usuario, correo, contraseña) VALUES (@nombre, @usuario, @correo, @contraseña)";
+                MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@correo", correo);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+                int resultado = Convert.ToInt32(comando.ExecuteScalar());
+                MessageBox.Show("Registro exitoso.");
+                Form1 nuevoFormulario = new Form1();
+                nuevoFormulario.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
